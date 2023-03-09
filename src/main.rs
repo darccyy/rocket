634183bb3dc2dd3872434f5deed34c1872803606
darcy <darcy3057@gmail.com>
2023-03-09 14:16:48 +1100

    Fix nightly definition

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,10 @@ use std::path::PathBuf;
 
 use rocket_cors::{AllowedHeaders, AllowedOrigins, Cors, CorsOptions};
 
+/// Default name returned if no name given
 const DEFAULT_NAME: &str = ":)";
 
+/// Get name, formatted, from optional raw name string
 fn get_name(name: Option<&str>) -> String {
     let name = name.unwrap_or(DEFAULT_NAME).replace('^', "ㅤ");
 
@@ -17,21 +19,25 @@ fn get_name(name: Option<&str>) -> String {
     format!(r#"{{"name": "{}"}}"#, name)
 }
 
+/// No query or path
 #[get("/")]
 fn index() -> String {
     get_name(None)
 }
 
+/// Query
 #[get("/?<name>")]
 fn name_query(name: String) -> String {
     get_name(Some(&name))
 }
 
+/// Path
 #[get("/<name..>")]
 fn name_path(name: PathBuf) -> String {
     get_name(name.to_str())
 }
 
+/// Get cors config
 fn get_cors() -> Cors {
     CorsOptions {
         allowed_origins: AllowedOrigins::All,

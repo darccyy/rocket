bdcfe1796cfca952403ff16df402bef3ba88bba8
darcy <darcy3057@gmail.com>
2023-03-09 13:26:07 +1100

    Add basic stuff

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
+use std::path::PathBuf;
+
 #[macro_use]
 extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, from Rocket!"
+const DEFAULT_NAME: &str = ":)";
+
+fn get_name(name: Option<&str>) -> String {
+    format!("Using name: '{}'", name.unwrap_or(DEFAULT_NAME))
+}
+
+#[get("/?<name>")]
+fn default(name: Option<&str>) -> String {
+    get_name(name)
+}
+
+#[get("/<name..>")]
+fn custom(name: PathBuf) -> String {
+    get_name(name.to_str())
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![default, custom])
 }
